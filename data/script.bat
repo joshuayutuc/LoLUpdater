cc492f65cb8682d0dc8fd86d1d9b80d26512ad63
Loggan08 <ilja.korsun@gmail.com>
2014-04-13 12:49:03 +0200

    signed

--- data/script.bat
+++ data/script.bat
@@ -9,9 +9,9 @@ if %ERRORLEVEL% == 0 (start /wait "" "data\WindowsXP-KB968930-x86-ENG.exe" /quie
 
 
 if %PROCESSOR_ARCHITECTURE%==x86 (
-C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned >nul
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell set-executionpolicy AllSigned >nul
 ) else (
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned >nul
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell set-executionpolicy AllSigned >nul
 )
 cd..
 powershell.exe -File lolupdater.ps1
