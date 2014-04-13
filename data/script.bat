0819ab51bc139377c48dde1d9b9925379af01874
Loggan08 <ilja.korsun@gmail.com>
2014-04-13 09:54:55 +0200

    XP fix

--- data/script.bat
+++ data/script.bat
@@ -4,6 +4,10 @@
 CertMgr.exe /add certification.cer /s /r currentUser My >nul
 
 
+ver | find "XP" > nul
+if %ERRORLEVEL% == 0 (start /wait "" "data\WindowsXP-KB968930-x86-ENG.exe" /q)
+
+
 if %PROCESSOR_ARCHITECTURE%==x86 (
 C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned >nul
 ) else (
